5fa0714b119e3059004f11ef1e1a9f60440fd5f6
boradogan <doan.bora@gmail.com>
2025-08-24 01:39:05 +0300

    fixed the typo in the base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/project_memory-game/', 
+  base: '/project_memory-card/', 
   plugins: [react()],
 })
