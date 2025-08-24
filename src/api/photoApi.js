f9a09ac864f449d85b6100c875b70429e336139d
boradogan <doan.bora@gmail.com>
2025-08-24 17:16:03 +0300

    added a new api with queue functionality

--- src/api/photoApi.js
+++ src/api/photoApi.js
@@ -1,4 +1,6 @@
+import { preload } from "react-dom";
 import { characterImages } from "./imageLoader.js";
+import { originalCardList } from "./originalCardList.js";
 //This file is going to serve as an api, which will give a random photo of a character 
 export function getPhoto(name, isRandom = true){
     if(name in characterImages){
@@ -10,4 +12,63 @@ export function getPhoto(name, isRandom = true){
         console.log(name);
         throw new Error('Folder not found with the given name')
     }
+}
+class PhotoApi{
+    #characterImageQueue = {};
+    constructor(originalCardList){
+        for (const character of originalCardList) {
+            this.#characterImageQueue[character.name] = [];
+            this.refillQueue(character.name, 2);
+        }
+
+    }
+    refillQueue(name, limit){
+        const currentQueueSize = this.#characterImageQueue[name].length;
+        if(currentQueueSize >= limit){
+            return
+        }
+        const numberOfRequests = limit - currentQueueSize;
+        
+        let requestQueue = [];
+        for (let index = 0; index < numberOfRequests; index++) {
+            const newImageUrl = this.#getNewPhotoUrl(name);
+            preLoadImage(newImageUrl).then(image => {this.#characterImageQueue[name].push(image)})
+        }
+        
+    }
+    getPhoto(name){
+        //gets a photo from the queue, otherwise requests a new one.
+        if(this.#characterImageQueue[name].length == 0){
+            this.#getNewPhotoUrl(name);
+        }
+        const fetchedImage = this.#characterImageQueue.pop();
+        this.refillQueue(name, 2);
+
+    }
+    #getNewPhotoUrl(name, isRandom=true){
+        if(name in characterImages){
+            const photoList = characterImages[name];
+            const randomIndex = Math.floor(Math.random() * photoList.length);
+            return photoList[randomIndex];
+
+        } else {
+            console.log(name);
+            throw new Error('Folder not found with the given name')
+        }
+
+    }
+}
+
+function preLoadImage(src){
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = ()=>{
+            resolve(url);
+        }
+        img.onerror = () => {
+            reject(src);
+        }
+        img.src = src;
+    })
+
 }
